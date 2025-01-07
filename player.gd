ab07f0b6ea9870a36bd3aecf0eb6ddf472d5a214
Axel Gehant <axelgehant42@gmail.com>
2025-01-07 15:44:45 +0100

    player + world

---
+++ player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+var look_dir: Vector2
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_dir = event.relative * 0.001
+		_rotate_camera()
+		
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _close() -> void:
+	get_tree().quit();
+	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("echap"):
+		_close()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+func _rotate_camera(sens_mod: float = 1.0) -> void:
+	$Camera3D.rotation.y -= look_dir.x * 1 * sens_mod
+	$Camera3D.rotation.x -= look_dir.y * 1 * sens_mod
