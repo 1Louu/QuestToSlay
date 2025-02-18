57b506bc18dadd2dc47b91ed557f57ccdbaeba44
Axel Gehant <axelgehant42@gmail.com>
2025-02-18 14:21:27 +0100

    Avancement joueur

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody3D
 var look_dir: Vector2
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var mob: Node3D 
+var can_attack = false
+
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+@export var ATTACK = 5
+@export var DEFENSE = 5
+@export var HP = 5
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -10,15 +17,34 @@ func _unhandled_input(event: InputEvent) -> void:
 		_rotate_camera()
 		
 func _ready() -> void:
+	$Area3D.connect("body_entered", _on_attack_range_body_entered)
+	$Area3D.connect("body_exited", _on_attack_range_body_exited)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+func _process(delta: float) -> void:
+	if can_attack and Input.is_action_just_pressed("attack"):
+		print("Mob hit!")
+	
+func _on_attack_range_body_entered(body: Node) -> void:
+	if body == mob:
+		can_attack = true
+		
+func _on_attack_range_body_exited(body: Node) -> void:
+	if body == mob:
+		can_attack = false
+
 func _close() -> void:
 	get_tree().quit();
+	
+func _isDead() -> void:
+	if (HP <= 0):
+		get_tree().change_scene_to_file("res://interfaces/mainmenu.tscn")
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
